594fa42607bf76c83f2d06151a5325c21f1db0c8
Akash671 <akashsaini45454@gmail.com>
2021-06-08 12:46:36 +0530

    real mathematics problems

---
+++ DailyChallenge/traling_zero.cpp
@@ -0,0 +1,32 @@
+/* author : @akash */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define ld long double
+
+
+void solve()
+{
+   int n;
+   cin>>n;
+   int ans=0;
+   for(int i=5;n/i>=1;i*=5)
+   {
+   	ans+=n/i;
+   }
+   cout<<ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    solve();
+    cout<<"\n";	    
+	return 0;
+}
